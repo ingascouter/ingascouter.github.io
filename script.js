f4e46492248fe416322172d3d8306191084b0ce2
Absolutely-Not-Me <erik.v.backman@gmail.com>
2025-01-05 02:04:08 +0200

    ayy I think I fixed #20 think I should sleep now

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-// Check for saved dark mode preference in localStorage
-const savedTheme = localStorage.getItem('theme');
-// If none, check browser theme preference
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-if (savedTheme === 'dark' || (!savedTheme && prefersDarkScheme)) {
-  enableDarkMode();
-} else {
-  disableDarkMode();
-}
-
-// Function to enable dark mode
-function enableDarkMode() {
-  // Set body "color scheme" style to "only dark"
-  document.body.style.colorScheme = 'dark';
-  // Set localStorage to dark
-  localStorage.setItem('theme', 'dark');
-}
-
-// Function to disable dark mode
-function disableDarkMode() {
-  // Set body "color scheme" style to "only light"
-  document.body.style.colorScheme = 'light';
-  // Set localStorage to light
-  localStorage.setItem('theme', 'light');
-}
-
-// Toggle dark mode function, called by button click, default to dark as page is light by default
-function toggleDarkMode() {
-  if (document.body.style.colorScheme === 'dark') {
-    disableDarkMode();
-  } else {
-    enableDarkMode();
-  }
-}
-
 // Hide navbar on scroll
 var prevScrollPos = window.scrollY;
 
