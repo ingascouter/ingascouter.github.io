dc368f37725249d40d896b02e99bfa6aca79f2e3
Backman Erik <erik.backman@edu.esbo.fi>
2025-01-10 10:49:04 +0200

    Revert "remove tabindex"
    This reverts commit 8e94448ad866c4632fb09200cc8b14c51da5f7a4.

--- theme.js
+++ theme.js
@@ -10,27 +10,19 @@ if (savedTheme === 'dark' || (!savedTheme && prefersDarkScheme)) {
 }
 
 // Function to enable dark mode
-const moon = document.querySelector('.moon');
 function enableDarkMode() {
-    // Set body "color scheme" style to "dark"
+    // Set body "color scheme" style to "only dark"
     document.documentElement.style.colorScheme = 'dark';
     // Set localStorage to dark
     localStorage.setItem('theme', 'dark');
-    // Update icon to match, by swapping "fa-sun" with "hidden" and changing "hidden" to "fa-moon"
-    moon.classList.remove('hidden');
-    moon.classList.add(modeIcons[currentModeIndex]);
 }
 
 // Function to disable dark mode
-const moon = document.querySelector('.sun');
 function disableDarkMode() {
-    // Set body "color scheme" style to "light"
+    // Set body "color scheme" style to "only light"
     document.documentElement.style.colorScheme = 'light';
     // Set localStorage to light
     localStorage.setItem('theme', 'light');
-    // Update icon to match, by swapping "fa-moon" with "hidden" and changing "hidden" to "fa-sun"
-    sun.classList.remove('hidden');
-    sun.classList.add(modeIcons[currentModeIndex]);
 }
 
 // Toggle dark mode function, called by button click, default to dark as page is light by default
